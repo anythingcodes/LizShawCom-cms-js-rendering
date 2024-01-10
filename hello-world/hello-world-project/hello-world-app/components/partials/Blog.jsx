14dcb76196fab728ff0454019c508f13c09f3c8b
Liz Shaw <liz.h.shaw@gmail.com>
2024-01-10 14:44:07 -0500

    Initial commit based on cms-js-building-block-examples

---
+++ hello-world/hello-world-project/hello-world-app/components/partials/Blog.jsx
@@ -0,0 +1,19 @@
+// TODO: Change CSS
+import homeStyles from '../../styles/home.module.css';
+
+import Layout from '../Layout.jsx';
+import HubSpotSprocket from '../HubSpotSprocket.jsx';
+
+function Blog() {
+  return (
+    <Layout>
+      <h1>Blog</h1>
+
+      {/* <div className={homeStyles.logo}>
+        <HubSpotSprocket link={'https://hubspot.com'} />
+      </div> */}
+    </Layout>
+  );
+}
+
+export default Blog;
