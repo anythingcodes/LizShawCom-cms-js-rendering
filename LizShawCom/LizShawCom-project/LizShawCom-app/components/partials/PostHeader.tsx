b80feeeaa900d18167e0646e49d35a27698d7412
Liz Shaw <liz.h.shaw@gmail.com>
2024-01-15 12:09:58 -0500

    Implement image placement for blog post header

--- LizShawCom/LizShawCom-project/LizShawCom-app/components/partials/PostHeader.tsx
+++ LizShawCom/LizShawCom-project/LizShawCom-app/components/partials/PostHeader.tsx
@@ -4,18 +4,30 @@ import StyledComponentsRegistry from '../StyledComponentsRegistry';
 import Tag, { type TagWithSlug } from './Tag';
 
 interface Props {
-  topic_list: string;
-  title: string;
-  subtitle: string;
-  image: string;
-  imageAlt?: string;
   authorDisplayName: string;
   authorSlug: string;
+  image: string;
+  imageAlt?: string;
+  imagePlacement: 'left' | 'right' | 'center';
   publishDate: string;
   publishedUrl: string;
+  subtitle: string;
+  title: string;
+  topic_list: string;
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ $imagePlacement: Props['imagePlacement'] }>`
+  margin: 0 auto;
+  ${({ $imagePlacement }) => {
+    if ($imagePlacement === 'center') {
+      return `
+      ${Column}:first-child {
+        margin-bottom: 32px;
+      }
+      `;
+    }
+    return null;
+  }}
   max-width: var(--max-width);
   padding: 0 var(--gutter-mobile);
   @media screen and (min-width: 768px) {
@@ -24,7 +36,23 @@ const Wrapper = styled.div`
     justify-content: space-between;
     padding-left: var(--gutter-desktop);
     padding-right: var(--gutter-desktop);
-  }
+    flex-direction: ${({ $imagePlacement }) =>
+      $imagePlacement === 'left' ? 'row-reverse' : null};
+      ${({ $imagePlacement }) => {
+        if ($imagePlacement === 'center') {
+          return `
+          flex-direction: column;
+          ${Column}:first-child {
+            max-width: var(--article-max-width);
+            margin: 0 auto 60px;
+          }
+          ${Column}:last-child {
+            width: 100%;
+          }
+          `;
+        }
+        return null;
+      }}
 `;
 
 const Column = styled.div`
@@ -41,8 +69,8 @@ const Meta = styled.div`
   font-size: var(--fs-5);
 `;
 
-const Author = styled.a`
-  &::after {
+const Time = styled.time`
+  &::before {
     content: '';
     position: relative;
     bottom: 0.2rem;
@@ -54,6 +82,7 @@ const Author = styled.a`
     background-color: #070e19;
   }
 `;
+
 const ImgWrapper = styled.div`
   position: relative;
   transform: translate(0);
@@ -61,7 +90,10 @@ const ImgWrapper = styled.div`
   border-radius: 8px;
   overflow: hidden;
   background: #eeeff1;
-  padding-top: 65%;
+  padding-top: 56.25%;
+  @media screen and (min-width: 768px) {
+    padding-top: 65%;
+  }
 `;
 
 const Img = styled.img`
@@ -79,6 +111,7 @@ const PostHeader = ({
   authorSlug,
   image,
   imageAlt,
+  imagePlacement,
   publishDate,
   publishedUrl,
   topic_list,
@@ -91,17 +124,27 @@ const PostHeader = ({
 
   return (
     <StyledComponentsRegistry>
-      <Wrapper>
+      <Wrapper $imagePlacement={imagePlacement}>
         <Column>
           <Tags>
             {tags.map(({ name, slug }) => (
-              <Tag slug={`/tag/${slug}`} name={name} />
+              <Tag
+                slug={`/tag/${slug}`}
+                name={name}
+                color={
+                  imagePlacement === 'left'
+                    ? 'purple'
+                    : imagePlacement === 'right'
+                    ? 'orange'
+                    : 'pink'
+                }
+              />
             ))}
           </Tags>
           <h1>{title}</h1>
           <Meta>
-            <Author href={`/${authorSlug}`}>{authorDisplayName}</Author>
-            <time dateTime={publishDate}>{computedDate}</time>
+            <a href={`/author/${authorSlug}`}>{authorDisplayName}</a>
+            <Time dateTime={publishDate}>{computedDate}</Time>
           </Meta>
         </Column>
         <Column>
