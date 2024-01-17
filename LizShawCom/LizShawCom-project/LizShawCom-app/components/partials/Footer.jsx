b17a3e5a4e9e3b3411c53e4ee70e85657c9ca1a8
Liz Shaw <liz.h.shaw@gmail.com>
2024-01-16 20:31:37 -0500

    Constrain footer width

--- LizShawCom/LizShawCom-project/LizShawCom-app/components/partials/Footer.jsx
+++ LizShawCom/LizShawCom-project/LizShawCom-app/components/partials/Footer.jsx
@@ -5,20 +5,22 @@ import StyledComponentsRegistry from '../StyledComponentsRegistry';
 const StyledFooter = styled.footer`
   text-align: center;
   font-size: var(--fs-5);
-  padding: 40px var(--gutter-mobile);
-  margin-top: 80px;
+  padding: 40px 0;
+  margin: 64px var(--gutter-mobile) 0 var(--gutter-mobile);
   border-top: 1px solid #eeeff1;
   color: #707584;
+  max-width: var(--max-width);
   @media screen and (min-width: 768px) {
-    padding-left: var(--gutter-desktop);
-    padding-left: var(--gutter-desktop);
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 80px;
   }
 `;
 
 const Footer = () => {
   return (
     <StyledComponentsRegistry>
-      {/* TODO: Build this */}
+      {/* TODO: Localize */}
       <StyledFooter>{new Date().getFullYear()} &copy; Liz Shaw. Built &amp; deployed on <a href="https://developers.hubspot.com/cms" target="_blank" rel="nofollow noreferrer">HubSpot CMS Hub</a>.</StyledFooter>
     </StyledComponentsRegistry>
   );
